3483ceaa0bdb87343d38294ad15ed6bf9363897e
Angana Chakraborty <43990458+Angana007@users.noreply.github.com>
2021-07-20 08:53:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,14 @@ from playsound import playsound
 
 if psutil.sensors_battery().power_plugged == True:
     if int(psutil.sensors_battery().percent)==100:
-        playsound('/home/angana/Downloads/drums.wav')
+        playsound('https://github.com/Angana007/battery-safe-or-unsafe/blob/main/drums.wav')
+    else:
+        exit()
+
+        
+        
+if psutil.sensors_battery().power_plugged == False:
+    if int(psutil.sensors_battery().percent)==20:
+        playsound('https://github.com/Angana007/battery-safe-or-unsafe/blob/main/drums.wav')
     else:
         exit()
